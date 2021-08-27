398080bf51b96f57d7159f199184724a255defd8
Mohd Syukri Bin Mohamed Jamil <mohd.syukri@S5.COM.MY>
2021-08-27 14:29:49 +0800

    2nd commit

---
+++ src/main/java/com/ezypay/ezypaySubscription/EzypaySubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.ezypay.ezypaySubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzypaySubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzypaySubscriptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezypay/ezypaySubscription/EzypaySubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezypay.ezypaySubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzypaySubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
